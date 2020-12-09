03b966f838ab43fe2e8dd991e5ebdd89aa69a9e7
Sarah <sarahjanelittlejohn@gmail.com>
2020-12-09 14:34:09 +0000

    Undo change environment variables to nfdiv

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,9 +8,9 @@ locals {
 
   idam_s2s_url              = "http://rpe-service-auth-provider-${local.local_env}.service.core-compute-${local.local_env}.internal"
   ccd_casedatastore_baseurl = "http://ccd-data-store-api-${local.local_env}.service.core-compute-${local.local_env}.internal"
-  case_formatter_baseurl    = "http://nfdiv-cfs-${local.local_env}.service.core-compute-${local.local_env}.internal"
+  case_formatter_baseurl    = "http://div-cfs-${local.local_env}.service.core-compute-${local.local_env}.internal"
   draft_store_api_baseurl   = "http://draft-store-service-${local.local_env}.service.core-compute-${local.local_env}.internal"
-  petitioner_fe_baseurl     = "https://nfdiv-pfe-${local.local_env}.service.core-compute-${local.local_env}.internal"
+  petitioner_fe_baseurl     = "https://div-pfe-${local.local_env}.service.core-compute-${local.local_env}.internal"
 
   previewVaultName    = "${var.raw_product}-aat"
   nonPreviewVaultName = "${var.raw_product}-${var.env}"
