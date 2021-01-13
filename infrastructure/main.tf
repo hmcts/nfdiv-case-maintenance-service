65b7fc7433f3efa527b2d558932f74e93ac23568
Damon Green <damon.green@btinternet.com>
2021-01-13 15:03:02 +0000

    remove $ from keyvault ids

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -53,11 +53,11 @@ data "azurerm_key_vault" "s2s_vault" {
 
 data "azurerm_key_vault_secret" "s2s_key" {
     name      = "microservicekey-nfdiv-cms"
-    key_vault_id = "${data.azurerm_key_vault.s2s_vault.id}"
+    key_vault_id = data.azurerm_key_vault.s2s_vault.id
 }
 
 resource "azurerm_key_vault_secret" "local_s2s_key" {
     name         = "cms-service-key"
     value        = "${data.azurerm_key_vault_secret.s2s_key.value}"
-    key_vault_id = "${data.azurerm_key_vault.div_key_vault.id}"
+    key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
