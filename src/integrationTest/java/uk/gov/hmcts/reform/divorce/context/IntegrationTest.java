584c55fbb3892db5ae6488b203110456261810a9
Linus Norton <linusnorton@gmail.com>
2021-01-21 21:00:00 +0000

    delete after all tests

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/context/IntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/context/IntegrationTest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationMethodRule;
 import org.junit.Rule;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -32,6 +33,18 @@ public abstract class IntegrationTest {
 
     protected IntegrationTest() {
         this.springMethodIntegration = new SpringIntegrationMethodRule();
+    }
+
+    @AfterAll
+    public void onDestroy() {
+        log.info("Destroying created users");
+        for (UserDetails details : createdUsers) {
+            try {
+                idamTestSupport.deleteUser(details.getEmailAddress());
+            } catch (Exception e) {
+                log.error("User deletion failed " + details.getEmailAddress(), e);
+            }
+        }
     }
 
     protected UserDetails getUserDetails() {
