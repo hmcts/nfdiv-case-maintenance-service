73515baf4f99ad588e2baf18c9258c7bed4f35a4
Linus Norton <linusnorton@gmail.com>
2021-02-09 12:10:24 +0000

    Revert "Add app insights (#30)"
    This reverts commit 982129f5d9130c3e91a549d128b5ee37896e7654.

--- src/test/java/uk/gov/hmcts/reform/divorce/casemaintenanceservice/controller/RootControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/casemaintenanceservice/controller/RootControllerTest.java
@@ -4,9 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -17,8 +15,7 @@ import uk.gov.hmcts.reform.divorce.casemaintenanceservice.CaseMaintenanceService
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(RootController.class)
-@ContextConfiguration(classes = CaseMaintenanceServiceApplication.class)
+@SpringBootTest(classes = CaseMaintenanceServiceApplication.class)
 @AutoConfigureMockMvc
 public class RootControllerTest {
 
